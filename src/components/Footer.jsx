947157e9a6e1f0adca64ebc988dd13352de079af
Aravind Jarpula <83942314+Aravind-Jarpula@users.noreply.github.com>
2021-06-25 19:54:17 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by Aravind_Jarpula @{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Info.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic boot camp about react and Javascript</p>
+      </div>
+    </div>
+  );
+}
+export default Info;
